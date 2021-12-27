b6b6a24fec21b4b6c37f71d64c31393d76c99841
alexvorodis <96724757+alexvorodis@users.noreply.github.com>
2021-12-27 15:14:17 +0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Practice </title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    
+    <div class="flex-container">
+
+        <div class="header">     
+               <?php include 'logo.inc.php' ?>         
+               <?php include 'menu.inc.php' ?>	   
+        </div>       
+     
+        <div class="about_me">
+         
+          <h1>  <?php  echo $p  ?> </h1>
+
+            <div class="data">
+                <div class="myImg">
+                    <?php  echo '<img src="img/php.jpg">'; ?>                    
+                </div>
+
+                <div class="fullname">
+                    <p> Меня зовут 
+                    <?php echo $name, ' ', $surname . '<br>'; 
+                          echo 'г.', ' ', $city; ?>                                      
+                    </p> 
+           
+                    <p> Мне
+                    <?php  echo $age;   ?>          
+                    лет </p>
+                    <p> Мы научились создавать переменные </p>
+                    <p> Изучили простые операции с ними </p>
+                </div>
+            </div>
+
+            <div class="knowledge">
+                                   
+                    <?php  include 'knowledge.inc.php'; ?>
+                    <?php   echo $a, ' ', $b, ' ', $c; ?> <br>
+                                       
+                    <?php
+                        $a = 10;
+                        $b = 20;
+                        $c = $a + $b;
+                        echo $c;
+                    ?>   <br>                
+                     <?php
+                        echo $d;
+                    ?> 
+
+            </div>
+
+            <div class="article">
+                <p class="text">
+                    Я пришел к тебе с приветом,<br>
+		    Рассказать, что солнце встало,<br>
+		    Что оно горячим светом<br>
+		    По листам затрепетало;<br>
+		    Рассказать, что лес проснулся,<br>
+		    Весь проснулся, веткой каждой,<br>
+		    Каждой птицей встрепенулся<br>
+		    И весенней полон жаждой;<br>
+		    Рассказать, что с той же страстью,<br>
+		    Как вчера, пришел я снова,<br>
+		    Что душа все так же счастью<br>
+		    И тебе служить готова;<br>
+		    Рассказать, что отовсюду<br>
+		    На меня весельем веет,<br>
+		    Что не знаю сам, что буду<br>
+		    Петь - но только песня зреет.<br>
+		    <br>
+					А.А.Фет<br>
+                </p>
+            </div>
+        </div>
+
+            <?php include 'footer.inc.php' ?>
+
+    </div>
+
+
+</body>
+</html>
